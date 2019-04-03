89094cff49cde1167a524dbc8faa71718b7651c3
Kevin Stokes <kevinstokes@kevins-mbp.attlocal.net>
2019-04-03 01:53:39 -0400

    finished functionality, updated styling, made mobile-responsive

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,6 @@ var listOfSports = ["football", "baseball", "basketball", "soccer", "hockey", "l
 // Function for displaying sports buttons
 function showButtons() {
 
-    // (this is necessary otherwise we will have repeat buttons)
     $("#button-holder").empty();
 
     // Looping through the array of sports
@@ -22,30 +21,32 @@ function showButtons() {
       $("#button-holder").append(newButton);
     }
   }
-  // end of show buttons function
-
   showButtons();
+  // end of show buttons function
 
       // adds a sport when you click the submit button
       $("#add-a-sport").on("click", function() {
-       // console.log("You clicked me yo");
         event.preventDefault();
         // This line will grab the text from the input box
         var sport = $("#new-sport-input").val().trim();
         // The sport the user entered is then added to our array
         listOfSports.push(sport);
+        console.log("Inside add sport");
+       // listOfSports.addClass("sport");
         // calling showButtons which updates the list in our array
         showButtons();
       });
 
+
 // event listener to add gifs to page, based on button you click
-$(".sport").on("click", function() {
+$(document).on("click","button", function(){
+//$(".sport").on("click", function() {
+  console.log("inside gif click");
     // make sure to clear out the div in the HTML prior to running each call back function
     $("#gifs-go-here").empty();
     var x = $(this).data("name");
-   // console.log(x)
+    console.log(x)
 
-// establish variables needed to create our AJAX call and to get something to return from the GIPHY API
 // when user clicks a button, the page should grab 10 static, non-animated gif images from the GIPHY API page and place them on the webpage
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=qz9Pt0PRRE8zgJLxiLo8sxiIEZQox6Ro&limit=10";
@@ -56,9 +57,10 @@ $(".sport").on("click", function() {
       // create a loop that appends a button to the page for each item in the variable  
         for (var j=0; j <response.data.length; j++) {
             var sportDiv = $("<div>");
-            var p = $("<h6>").html("Rating: " + response.data[j].rating + "<br>" + "Title: " + response.data[j].title + "<br>" + "Source: " + response.data[j].source);
+            sportDiv.addClass("col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6");
+            var p = $("<h5>").html("Rating: " + response.data[j].rating + "<br>" + "Title: " + response.data[j].title); // + "<br>" + "Source: " + response.data[j].source);
             var sportImage = $("<img>"); 
-            sportImage.addClass("giphy-sport-image");
+            sportImage.addClass("giphy-sport-image", "img-responsive");
 			      sportImage.attr("state", "still");
 			      sportImage.attr("still-data", response.data[j].images.fixed_height_still.url);
 			      sportImage.attr("animated-data", response.data[j].images.fixed_height.url);
@@ -67,10 +69,13 @@ $(".sport").on("click", function() {
             sportDiv.append(sportImage); 
             $("#gifs-go-here").append(sportDiv);
         }
+      });  
 
+ });
         // create function to make GIF animate when user clicks on it, or to stop animation when user clicks on it again
 
-        $(".giphy-sport-image").on("click", function(){
+        $(document).on("click",".giphy-sport-image",function(){
+        //$(".giphy-sport-image").on("click", function(){
            // console.log(this);
             if($(this).attr("state") === "still") {
                 $(this).attr("state", "animated");
@@ -80,5 +85,5 @@ $(".sport").on("click", function() {
                 $(this).attr("state", "still");
                 $(this).attr("src", $(this).attr("still-data"));
             }})
-    })
-    })
+   
+   
